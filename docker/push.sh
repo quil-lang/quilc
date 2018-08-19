ce456022356e99c3452edab55c3c2319a0f8f74c
Peter Karalekas <peter@rigetti.com>
2018-08-19 13:46:57 -0700

    Add branch tagging to the docker push.sh script so we're not overwriting latest

--- docker/push.sh
+++ docker/push.sh
@@ -3,7 +3,23 @@ set -ex
 ###############################################################################
 # Script for tagging and pushing Docker images to our internal Harbor
 ###############################################################################
-if [ -z "$bamboo_DOCKER_IMAGE_TAG" ]; then bamboo_DOCKER_IMAGE_TAG="latest"; fi
+GIT_BRANCH_NAME=`git name-rev --name-only HEAD | cut -d~ -f1 | tr "/" "-"`
+if [ "$GIT_BRANCH_NAME" = "master" ] && [ -z "$bamboo_DOCKER_IMAGE_TAG" ]
+then
+    echo "Standard build on master, so using the 'latest' tag"
+    bamboo_DOCKER_IMAGE_TAG="latest"
+elif [ "$GIT_BRANCH_NAME" = "master" ]
+then
+    echo "Custom build on master, so using overridden bamboo tag"
+elif [ "$GIT_BRANCH_NAME" != "master" ] && [ -z "$bamboo_DOCKER_IMAGE_TAG" ]
+then
+    echo "Standard build on a branch, so using the branch name as tag"
+    bamboo_DOCKER_IMAGE_TAG="$GIT_BRANCH_NAME"
+elif [ "$GIT_BRANCH_NAME" != "master" ]
+then
+    echo "Custom builds on branches not supported, falling back to branch name as tag"
+    bamboo_DOCKER_IMAGE_TAG="$GIT_BRANCH_NAME"
+fi
 echo "Tagging Docker image with: $bamboo_DOCKER_IMAGE_TAG"
 docker tag quilc docker.lab.rigetti.com/qcs/quilc:$bamboo_DOCKER_IMAGE_TAG
 docker push docker.lab.rigetti.com/qcs/quilc:$bamboo_DOCKER_IMAGE_TAG
