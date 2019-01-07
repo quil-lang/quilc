d183cc33fd269bf90a903a6e439291719dd697b4
Mark Skilbeck <mark.skilbeck@rigetti.com>
2019-01-07 14:03:05 -0800

    Move some stuff from chip-spec into utils

--- src/utilities.lisp
+++ src/utilities.lisp
@@ -7,3 +7,21 @@
 (defun required-slot (slot-name)
   (check-type slot-name symbol)
   (error "The slot named ~S is required." slot-name))
+
+(defmacro postpend (obj place)
+  `(if ,place
+       (push ,obj (cdr (last ,place)))
+       (setf ,place (list ,obj))))
+
+(defun make-adjustable-vector ()
+  (make-array 4 :element-type t
+                :initial-element nil
+                :adjustable t
+                :fill-pointer 0))
+
+(defun vnth (index vector)
+  "Like NTH, but for VECTORs."
+  (aref vector index))
+
+(defun (setf vnth) (val index vector)
+  (setf (aref vector index) val))
