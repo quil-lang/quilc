e1e1ad1cfba093a34f5a37552c284f8db9d1a652
Peter Karalekas <peter@rigetti.com>
2019-01-09 13:49:58 -0800

    Remove the docker subdirectory

--- docker/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -ex
-###############################################################################
-# Script for running dockerized quilc tests
-###############################################################################
-docker run --rm --name quilc-tests quilc-tests
