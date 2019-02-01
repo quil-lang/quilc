1a1fcad4c61492afde5f6a2545e8539095900e51
Mark Skilbeck <markskilbeck@gmail.com>
2019-02-01 11:32:50 -0800

    do push current-type to higher-precedence in build-link/qubit

--- src/chip-specification.lisp
+++ src/chip-specification.lisp
@@ -308,6 +308,7 @@ MISC-DATA is a hash-table of miscellaneous data associated to this hardware obje
                          (not (find ':ISWAP  higher-precedence)))
                     (list #'ISWAP-to-PISWAP)))
             :into compilation-methods
+          :do (push current-type higher-precedence)
           :finally
              (setf (gethash "duration-alist" misc-data)
                    (nconc duration-alist (gethash "duration-alist" misc-data)))
@@ -390,6 +391,7 @@ MISC-DATA is a hash-table of miscellaneous data associated to this hardware obje
                           #'RX-to-ZXZXZ
                           #'euler-compiler)))
             :into compilation-methods
+          :do (push current-type higher-precedence)
           :finally
              (setf (gethash "duration-alist" misc-data)
                    (nconc duration-alist (gethash "duration-alist" misc-data)))
