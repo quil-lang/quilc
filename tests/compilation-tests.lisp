83c137405ca315eb82d09c3c0b31018116e7ec87
Mark Skilbeck <mark.skilbeck@rigetti.com>
2019-02-12 12:49:25 -0800

    gate-apps-to-log-mat -> proc-prog-to-log-mat

--- tests/compilation-tests.lisp
+++ tests/compilation-tests.lisp
@@ -219,9 +219,9 @@ ISWAP 5 2"))
 CNOT 1 0
 CNOT 2 1
 CNOT 0 2"))
-         (orig-matrix (quil::gate-applications-to-logical-matrix orig-prog))
+         (orig-matrix (quil::parsed-program-to-logical-matrix orig-prog))
          (proc-prog (quil::compiler-hook orig-prog chip))
-         (proc-matrix (quil::gate-applications-to-logical-matrix proc-prog))
+         (proc-matrix (quil::parsed-program-to-logical-matrix proc-prog))
          (2q-code (program-2q-instructions proc-prog)))
     (is (matrix-equals-dwim orig-matrix proc-matrix))
     (is (every (link-nativep chip) 2q-code))))
