016b5cfc16270adf7f7b6f827f0ce4a41bb216e0
Robert Smith <robert@rigetti.com>
2019-03-17 20:27:40 -0700

    lift transforms of PARSE-QUIL-STRING into variable

--- src/cl-quil.lisp
+++ src/cl-quil.lisp
@@ -4,17 +4,17 @@
 
 (in-package #:cl-quil)
 
+(defvar *standard-post-process-transforms*
+  '(process-includes resolve-applications expand-circuits type-check patch-labels)
+  "The standard transforms that are applied by PARSE-QUIL-STRING.")
+
 (defun parse-quil-string (string &optional originating-file)
   "Parse and process the Quil string STRING, which originated from the file ORIGINATING-FILE."
   (let ((pp (parse-quil string)))
-    (setf pp (transform 'process-includes pp originating-file))
-    (setf pp (transform 'resolve-applications pp))
-    (setf pp (transform 'expand-circuits pp))
-    (setf pp (transform 'type-check pp))
-    ;; XXX: FIXME, the QVM should probably do this on its own
-    ;; accord. Figure it out! Don't merge until this is fixed.
-    (setf pp (transform 'patch-labels pp))
-    pp))
+    (dolist (xform *standard-post-process-transforms* pp)
+      (case xform
+        (process-includes (setf pp (transform xform pp originating-file)))
+        (otherwise        (setf pp (transform xform pp)))))))
 
 (defun read-quil-file (filespec)
   "Read the Quil file designated by FILESPEC, and parse it as if by PARSE-QUIL-STRING."
