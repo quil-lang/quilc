1fb7dfbdfeaee05d20f953e6d12e1d94af91e896
Mike Appleby <86076+appleby@users.noreply.github.com>
2019-04-05 17:52:33 -0600

    Fixes for test-faithfulness in quilc-tests
    1) Fix the path to the *faithfulness-test-file-directory*
    
    The invalid path was causing uiop:directory-files to return nil, which
    resulted in the test passing without actually testing any files.
    
    2) Ensure that the test fails if no test files are found
    
    3) Pass -P not -p to quilc::%entry-point
    
    Lowercase -p is --port. Uppercase -P is --protoquil, which is required
    by -m (--compute-matrix-reps). Apparently, -m without -P has no
    effect.
    
    4) Bind quilc::*protoquil* and quilc::*compute-matrix-reps* before
    calling quilc::%entry-point. The "-mP" flags cause these variables to
    be set, which causes some downstream test to fail if the global
    bindings are modified.
    
    5) Assert that quilc reports that the computed matrices are equal.

--- app/tests/faithfulness-tests.lisp
+++ app/tests/faithfulness-tests.lisp
@@ -2,23 +2,33 @@
 ;;;;
 ;;;; Author: Eric Peterson
 ;;;;
-;;;; Ensures that quilc's output matches its input,, according to the -m switch.
+;;;; Ensures that quilc's output matches its input, according to the -m switch.
 
 (in-package #:quilc-tests)
 
 (defparameter *faithfulness-test-file-directory*
   (asdf:system-relative-pathname
    ':quilc-tests
-   "tests/faithfulness-test-files/"))
+   "app/tests/faithfulness-test-files/"))
 
 (deftest test-faithfulness ()
   "Test whether compilation preserves semantic equivalence for some test programs."
   (finish-output *debug-io*)
-  (dolist (file (uiop:directory-files *faithfulness-test-file-directory* #P"*.quil"))
-    (format *debug-io* "    Testing file ~a~%" (pathname-name file))
-    (let ((*error-output* (make-broadcast-stream))
-          (*standard-output* (make-broadcast-stream)))
-      (with-open-file (*standard-input* file :direction :input)
-        (locally
-            (declare #+sbcl(sb-ext:muffle-conditions common-lisp:style-warning))
-          (quilc::%entry-point (list "quilc" "-mp")))))))
+  (let ((test-files (uiop:directory-files *faithfulness-test-file-directory* #P"*.quil")))
+    (is (not (null test-files)))
+    (fresh-line)
+    (dolist (file test-files)
+      (format t "    Testing file ~a~%" (pathname-name file))
+      (let ((*standard-output* (make-broadcast-stream))
+            ;; quilc::process-options sets *protoquil* and *compute-matrix-reps* when passed "-mP"
+            ;; flags. Bind them here so that the global bindings are not affected; otherwise,
+            ;; subsequent tests that depend on the default values will fail.
+            quilc::*protoquil*
+            quilc::*compute-matrix-reps*)
+        (is (search "#Matrices are equal"
+                    (with-open-file (*standard-input* file :direction :input)
+                      (with-output-to-string (*error-output*)
+                        (locally
+                            (declare #+sbcl(sb-ext:muffle-conditions style-warning))
+                          (quilc::%entry-point (list "quilc" "-mP")))))
+                    :from-end t))))))
