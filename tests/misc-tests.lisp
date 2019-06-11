fb44a1bf60b4419039536f698c2f3cb197ecd590
Mark Skilbeck <mark.skilbeck@rigetti.com>
2019-06-11 10:04:42 -0700

    use instruction-fmt

--- tests/misc-tests.lisp
+++ tests/misc-tests.lisp
@@ -44,39 +44,34 @@
 
 (deftest test-print-instruction ()
   (is (string= "PRAGMA gate_time CNOT \"50 ns\""
-               (with-output-to-string (s)
-                 (cl-quil::print-instruction (make-instance 'quil::pragma
-                                                            :words '("gate_time" "CNOT")
-                                                            :freeform-string "50 ns")
-                                             s))))
+               (format nil "~/cl-quil:instruction-fmt/"
+                       (make-instance 'quil::pragma
+                                      :words '("gate_time" "CNOT")
+                                      :freeform-string "50 ns"))))
   ;; try a operand-free instruction
   (is (string= "HALT"
-               (with-output-to-string (s)
-                 (cl-quil::print-instruction (make-instance 'halt)
-                                             s))))
+               (format nil "~/cl-quil:instruction-fmt/"
+                       (make-instance 'halt))))
   ;; try a unary instruction
   (is (string= "NEG ro[3]"
-               (with-output-to-string (s)
-                 (cl-quil::print-instruction (make-instance 'quil::classical-negate
-                                                            :target (mref "ro" 3))
-                                             s))))
+               (format nil "~/cl-quil:instruction-fmt/"
+                       (make-instance 'quil::classical-negate
+                                      :target (mref "ro" 3)))))
   ;; try a binary instruction
   (is (string= "MEASURE 1 ro[3]"
-               (with-output-to-string (s)
-                 (cl-quil::print-instruction (make-instance 'quil::measure
-                                                            :address (mref "ro" 3)
-                                                            :qubit (qubit 1))
-                                             s))))
+               (format nil "~/cl-quil:instruction-fmt/"
+                       (make-instance 'quil::measure
+                                      :address (mref "ro" 3)
+                                      :qubit (qubit 1)))))
   ;; try something fancy
   (is (string= "CPHASE-AND-MEASURE(%alpha) 1 3 ro[5]"
-               (with-output-to-string (s)
-                 (cl-quil::print-instruction (make-instance 'cl-quil::circuit-application
-                                                            :operator #.(named-operator "CPHASE-AND-MEASURE")
-                                                            :parameters `(,(param "alpha"))
-                                                            :arguments `(,(qubit 1)
-                                                                         ,(qubit 3)
-                                                                         ,(mref "ro" 5)))
-                                             s)))))
+               (format nil "~/cl-quil:instruction-fmt/"
+                       (make-instance 'cl-quil::circuit-application
+                                      :operator #.(named-operator "CPHASE-AND-MEASURE")
+                                      :parameters `(,(param "alpha"))
+                                      :arguments `(,(qubit 1)
+                                                   ,(qubit 3)
+                                                   ,(mref "ro" 5)))))))
 
 (deftest test-big-defgate ()
   (let* ((qubit-count 8)
