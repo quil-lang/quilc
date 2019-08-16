8c9e1902bdad7357ffb446759a3e30eff1b31da0
Chris Osborn <chris@rigetti.com>
2019-08-15 18:00:40 -0700

    Indentation.

--- boondoggle/tests/tests.lisp
+++ boondoggle/tests/tests.lisp
@@ -20,30 +20,29 @@ Note that the processors are indexed by compiled/uncompiled in the variable i, a
 The processor-two-sample-chi-squared  post-process takes all consumers and processors as input (a.k.a. the qvm results and quil programs, respectively), and generates as output the chi-squared statistics (bundled with their categorical degrees of freedom) between all possible combinations."
   (let ((boondoggle::*debug-noise-stream* *standard-output*)
         (chip-spec (quil::build-8q-chip)))
-        (let ((chi-squared-results (boondoggle::pipeline
-                        ((producer      ()        (make-instance 'boondoggle::producer-random
-                                                                 :program-volume-limit 20
-                                                                 :chip-specification chip-spec
-                                                                 :respect-topology t))      
-                         (processors    (i)       (list (make-instance 'boondoggle::processor-identity)
-                                                        (make-instance 'boondoggle::processor-quilc
-                                                                       :executable-path (namestring #P"./quilc"))))
-                         (consumer      ()        (make-instance 'boondoggle::consumer-local-qvm
-                                                                 :trials 1000))
-                         (post-process  ()        (make-instance 'boondoggle::processor-two-sample-chi-squared)))
-                        (produced-program ()
-                                          (boondoggle::produce-quil-program (producer)))
-                        (compiled-program ((i processors))
-                                          (progn
-                                            (quil::print-parsed-program (produced-program))
-                                            (boondoggle::apply-process (processors i) (produced-program))))
-                        (qvm-results ((i processors))
-                                     (progn
-                                       (quil::print-parsed-program (compiled-program i))
-                                       (boondoggle::consume-quil (consumer) (compiled-program i))))
-                        (chi-squared-result ((k processors) (i processors))
-                                            (boondoggle::apply-process (post-process) (qvm-results i) (qvm-results k)))
-                        )))
+        (let ((chi-squared-results
+                (boondoggle::pipeline
+                  ((producer      ()        (make-instance 'boondoggle::producer-random
+                                                           :program-volume-limit 20
+                                                           :chip-specification chip-spec
+                                                           :respect-topology t))      
+                   (processors    (i)       (list (make-instance 'boondoggle::processor-identity)
+                                                  (make-instance 'boondoggle::processor-quilc
+                                                                 :executable-path (namestring #P"./quilc"))))
+                   (consumer      ()        (make-instance 'boondoggle::consumer-local-qvm
+                                                           :trials 1000))
+                   (post-process  ()        (make-instance 'boondoggle::processor-two-sample-chi-squared)))
+                  (produced-program () (boondoggle::produce-quil-program (producer)))
+                  (compiled-program ((i processors))
+                                    (progn
+                                      (quil::print-parsed-program (produced-program))
+                                      (boondoggle::apply-process (processors i) (produced-program))))
+                  (qvm-results ((i processors))
+                               (progn
+                                 (quil::print-parsed-program (compiled-program i))
+                                 (boondoggle::consume-quil (consumer) (compiled-program i))))
+                  (chi-squared-result ((k processors) (i processors))
+                                      (boondoggle::apply-process (post-process) (qvm-results i) (qvm-results k))))))
       (destructuring-bind (((chi-1 deg-1) (chi-2 deg-2)) ((chi-3 deg-3) (chi-4 deg-4))) chi-squared-results ; chi-squared-results are of the form e.g. ((0.0d0 0.5d0) (0.5d0 0.0d0))
         (format t "chi-squared-results: ~A" chi-squared-results)
         (fiasco:is (= chi-1 chi-4)) ; Off-diagonal elements must equal
