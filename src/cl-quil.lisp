10af0065c4393fb179f7cb010ab84e3966882ec2
Erik Davis <erik@rigetti.com>
2019-09-24 13:02:48 -0700

    move RAW-QUIL-TO-UNRESOLVED-PROGRAM

--- src/cl-quil.lisp
+++ src/cl-quil.lisp
@@ -19,6 +19,93 @@
         (quil-parse-error "Memory region ~A~@[ (in ~A)~] has already been DECLAREd~@[ (in ~A)~]."
                           name file other-file)))))
 
+(defun definition-signature (instr)
+  "If INSTR is a definition or memory declaration, returns a signature used for
+determining ambiguity. Otherwise, return NIL."
+  (flet ((gate-or-circuit-signature (name params args)
+           ;; TODO in principle the signature should include the number of params and args
+           ;; since e.g. we can tell the difference between FOO 1 and FOO 1 2. however,
+           ;; for now we follow the convention of not discriminating between these.
+           (declare (ignore params args))
+           (list 'gate-or-circuit-definition
+                 name)))
+    (typecase instr
+      (gate-definition (gate-or-circuit-signature (gate-definition-name instr)
+                                                  (if (typep instr 'parameterized-gate-definition)
+                                                      (gate-definition-parameters instr)
+                                                      nil)
+                                                  (gate-definition-qubits-needed instr)) )
+      (circuit-definition (gate-or-circuit-signature (circuit-definition-name instr)
+                                                     (circuit-definition-parameters instr)
+                                                     (circuit-definition-arguments instr)))
+      (waveform-definition (cons 'waveform-definition
+                                 (waveform-definition-name instr)))
+      ;; TODO actually think through what these signatures should be like
+      (gate-calibration-definition (list 'gate-calibration-definition
+                                         (calibration-definition-operator instr)))
+      (measurement-calibration-definition (list 'measurement-calibration-definition))
+      ;; TODO (frame-definition (list 'frame-definition (frame-definition-frame instr)))
+      (memory-descriptor (cons 'memory-descriptor
+                               (memory-descriptor-name instr))))))
+
+(defun ambiguous-definition-condition (instr file conflicts)
+  "Signal a condition indicating that the instruction INSTR parsed from FILE has
+a list of conflicts CONFLICTS."
+  (let ((combined (acons instr file conflicts)))
+    (etypecase instr
+      (gate-definition (make-condition 'ambiguous-gate-or-circuit-definition :conflicts combined))
+      (circuit-definition (make-condition 'ambiguous-gate-or-circuit-definition :conflicts combined))
+      (waveform-definition (make-condition 'ambiguous-waveform-definition :conflicts combined))
+      (calibration-definition (make-condition 'ambiguous-calibration-definition :conflicts combined))
+      ;; TODO frame definition
+      (memory-descriptor (make-condition 'ambiguous-memory-declaration :conflicts combined)))))
+
+(defun raw-quil-to-unresolved-program (code)
+  "This constructes a PARSED-PROGRAM object from the given quil CODE, without any resolution of applications.
+
+This also signals ambiguous definitions, which may be handled as needed."
+  ;; Note: this preserves the order of definitions.
+  (let ((gate-defs nil)
+        (circ-defs nil)
+        (memory-defs nil)
+        (wf-defs nil)
+        (calib-defs nil)
+        (frame-defs nil)
+        (exec-code nil)
+        ;; The following maps definition signatures to a list of (filename . defn) pairs
+        (all-seen-defns (make-hash-table :test 'equal)))
+    (flet ((bin (instr)
+             (a:when-let ((signature (definition-signature instr)))
+               (let ((originating-file (typecase (lexical-context instr)
+                                         (token
+                                          (token-pathname (lexical-context instr)))
+                                         (t
+                                          (quil-parse-error "Unable to resolve definition context ~A" instr)))))
+                 ;; check for conflicts
+                 (a:when-let ((entries (gethash signature all-seen-defns)))
+                   (cerror "Continue with ambiguous definition."
+                           (ambiguous-definition-condition instr originating-file entries)))
+                 (push (cons instr originating-file)
+                       (gethash signature all-seen-defns))))
+             (typecase instr
+               (gate-definition (push instr gate-defs))
+               (circuit-definition (push instr circ-defs))
+               (waveform-definition (push instr wf-defs))
+               (calibration-definition (push instr calib-defs))
+               (frame-definition (push instr frame-defs))
+               (memory-descriptor (push instr memory-defs))
+               (t (push instr exec-code)))))
+      (mapc #'bin code)
+      (make-instance 'parsed-program
+                     :gate-definitions (nreverse gate-defs)
+                     :circuit-definitions (nreverse circ-defs)
+                     :waveform-definitions (nreverse wf-defs)
+                     :calibration-definitions (nreverse calib-defs)
+                     :frame-definitions (nreverse frame-defs)
+                     :memory-definitions (nreverse memory-defs)
+                     :executable-code (coerce (nreverse exec-code)
+                                              'simple-vector)))))
+
 (defun parse-quil (string &key originating-file
                             (transforms *standard-post-process-transforms*)
                             (ambiguous-definition-handler #'continue))
