563165149d91f6c9c36f35b596eb31a5d1041e09
Eric Peterson <eric@rigetti.com>
2019-11-18 11:55:58 -0500

    add a weak test for the different addresser types

--- tests/addresser-tests.lisp
+++ tests/addresser-tests.lisp
@@ -14,3 +14,14 @@
              (quil::parsed-program-to-logical-matrix
               (quil::compiler-hook (quil::parse-quil text) (quil::build-8q-chip)
                                    :protoquil nil :rewiring-type ':naive))))))))
+
+(deftest test-addresser-subclasses ()
+  (let* ((chip (quil::build-8q-chip))
+         (text "CNOT 0 1 ; CNOT 0 2 ; CNOT 1 2")
+         (orig-mat (quil::parsed-program-to-logical-matrix (quil::parse-quil text))))
+    (dolist (quil::*default-addresser-state-class*
+             (list 'quil::temporal-addresser-state
+                   'quil::fidelity-addresser-state))
+      (let* ((pp (quil::parse-quil text))
+             (cpp (quil::compiler-hook pp chip :protoquil t)))
+        (is (quil::operator= orig-mat (quil::parsed-program-to-logical-matrix cpp)))))))
