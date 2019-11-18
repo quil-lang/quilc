62ae9516fb2c8225b7dbcbd19533feb9a09e7495
Mark Skilbeck <mark.skilbeck@rigetti.com>
2019-11-18 10:14:31 -0800

    Remove redundant test, rename test

--- tests/rewrite-tests.lisp
+++ tests/rewrite-tests.lisp
@@ -8,17 +8,19 @@
   ;; Test that the compiler takes effect via compiler-hook.
   (let ((chip (quil::build-nq-fully-connected-chip 2))
         (pps (list (quil::parse-quil "DECLARE theta REAL[1]; RZ(0.0) 0;")
-                   (quil::parse-quil "DECLARE theta REAL[1]; RZ(theta) 0; RZ(-theta) 0;"))))
+                   (quil::parse-quil "DECLARE theta REAL[1]; RZ(2*pi) 0;")
+                   (quil::parse-quil "DECLARE theta REAL[1]; RZ(-2*pi) 0;"))))
     (dolist (pp pps)
       (is (zerop (length (parsed-program-executable-code
                           (quilc::process-program pp chip :protoquil t))))))))
 
-(deftest test-rz-agglutination ()
+(deftest test-rz-agglutination-elimination ()
   (let ((chip (quil::build-nq-fully-connected-chip 2))
         (pps (list
               (parse-quil "RZ(1) 0; RZ(-1) 0;")
               (parse-quil "DECLARE theta REAL[1]; RZ(theta) 0; RZ(-theta) 0"))))
     (dolist (pp pps)
-      (is (zerop (length (%filter-halt (quil::compiler-hook pp chip))))))))
+      (is (zerop (length (parsed-program-executable-code
+                          (quilc::process-program pp chip :protoquil t))))))))
 
 
