b2ac69c67d9741ccfe34e5fbdb4c7b2beea13b4b
Juan M. Bello-Rivas <jbellorivas@rigetti.com>
2019-11-24 23:30:27 -0800

    Implement CS decomposition for the partition (p, q) = (1, 1).

--- src/csd.lisp
+++ src/csd.lisp
@@ -4,11 +4,11 @@
 
 (in-package #:cl-quil)
 
-;;; Compute the 2x2 Cosine-Sine decomposition of an unitary matrix following:
-;;; E. S. Gawlik, Yuji. Nakatsukasa, and B. D. Sutton, “A Backward Stable
-;;; Algorithm for Computing the CS Decomposition via the Polar
-;;; Decomposition,” SIAM J. Matrix Anal. Appl., vol. 39, no. 3,
-;;; pp. 1448–1469, Jan. 2018. DOI:10.1137/18M1182747
+;;; Compute the 2x2 Cosine-Sine decomposition of an unitary matrix using a
+;;; modification of the algorithm in: E. S. Gawlik, Yuji. Nakatsukasa, and
+;;; B. D. Sutton, “A Backward Stable Algorithm for Computing the CS
+;;; Decomposition via the Polar Decomposition,” SIAM J. Matrix Anal. Appl.,
+;;; vol. 39, no. 3, pp. 1448–1469, Jan. 2018. DOI:10.1137/18M1182747
 
 (defun m*-diag-general (diag matrix)
   "Returns a newly allocated matrix resulting from the product of DIAG (a diagonal real matrix) with MATRIX (a complex matrix)."
@@ -18,7 +18,6 @@
          (n (magicl:matrix-cols diag))
          (k (magicl:matrix-cols matrix))
          (result (magicl:make-zero-matrix m k)))
-    (assert (= n k))
     (dotimes (i (min m n) result)
       (let ((dii (magicl:ref diag i i)))
         (dotimes (j k)
@@ -41,33 +40,35 @@
                          (m* u (m*-diag-general sigma (magicl:conjugate-transpose u)))))))
       (values unitary hermitian))))
 
-(defun csd-2x1 (a1 a2)
+(defun csd-2x1 (a3 a4)
   "Compute the 2x1 Cosine-Sine decomposition of a unitary matrix A partitioned into blocks A1 A2 as shown below:
 
-       ⎡ A1 ⎤        ⎡ A1 ⎤   ⎡ U1     ⎤ ⎡ C ⎤
-If A = ⎢    ⎥, then  ⎢    ⎥ = ⎢        ⎥ ⎢   ⎥ V1ᴴ,
-       ⎣ A2 ⎦        ⎣ A2 ⎦   ⎣     U2 ⎦ ⎣ S ⎦
+       ⎡ A3 ⎤        ⎡ A3 ⎤   ⎡ U1     ⎤ ⎡ -S ⎤
+If A = ⎢    ⎥, then  ⎢    ⎥ = ⎢        ⎥ ⎢    ⎥ V2ᴴ,
+       ⎣ A4 ⎦        ⎣ A4 ⎦   ⎣     U2 ⎦ ⎣  C ⎦
 
-where U1, U2, and V1 are unitary and C^2 + S^2 = I."
+where U1, U2, and V2 are unitary and C^2 + S^2 = I."
+  ;; XXX update documentation.
   (multiple-value-bind (w1 h1)
-      (polar-decomposition a1 :mode :unitary-hermitian)
+      (polar-decomposition a3 :mode :unitary-hermitian)
 
     (multiple-value-bind (w2 h2)
-        (polar-decomposition a2 :mode :unitary-hermitian)
+        (polar-decomposition a4 :mode :unitary-hermitian)
 
       (let ((b (m- h2 h1)))
-        (multiple-value-bind (lambda1 v1)
+        (multiple-value-bind (lambda2 v2)
             (magicl:hermitian-eig b)
-          (declare (ignorable lambda1))
+          (declare (ignorable lambda2))
 
-          (let* ((u1 (m* w1 v1))
-                 (u2 (m* w2 v1))
-                 (v1h (magicl:conjugate-transpose v1))
-                 (c (m* v1h h1 v1))
-                 (s (m* v1h h2 v1)))
-            (values u1 u2 c s v1)))))))
+          (let* ((u1 (m- (m* w1 v2)))   ; Absorb the negative sign in the unitary matrix.
+                 (u2 (m* w2 v2))
+                 (v2h (magicl:conjugate-transpose v2))
+                 (s (m* v2h h1 v2))
+                 (c (m* v2h h2 v2)))
 
-(defun csd-2x2 (matrix)
+            (values u1 u2 c s v2h)))))))
+
+(defun csd (matrix p q)
   "Compute the 2x2 Cosine-Sine decomposition of MATRIX (assumed to be unitary and 2n×2n) partitioned into n×n blocks A1 A2 A3 A4 as shown below:
 
        ⎡ A1  A3 ⎤        ⎡ A1  A3 ⎤   ⎡ U1     ⎤ ⎡ C  -S ⎤ ⎡ V1     ⎤H
@@ -75,39 +76,55 @@ If A = ⎢        ⎥, then  ⎢        ⎥ = ⎢        ⎥ ⎢       ⎥ ⎢  
        ⎣ A2  A4 ⎦        ⎣ A2  A4 ⎦   ⎣     U2 ⎦ ⎣ S   C ⎦ ⎣     V2 ⎦
 
 where U1, U2, V1, and V2 are unitary and C^2 + S^2 = I."
+  ;; XXX update documentation.
   (declare (type magicl:matrix matrix)
-           (values magicl:matrix magicl:matrix magicl:matrix magicl:matrix))
-  (let* ((m (magicl:matrix-rows matrix))
-         (n (/ m 2))
-         (a1 (magicl:slice matrix 0 n 0 n))
-         (a2 (magicl:slice matrix n m 0 n))
-         (a3 (magicl:slice matrix 0 n n m))
-         (a4 (magicl:slice matrix n m n m)))
+           (values magicl:matrix magicl:matrix magicl:matrix magicl:matrix list))
 
-    (multiple-value-bind (u1 u2 c s v1)
-        (csd-2x1 a1 a2)
+  (let ((m (magicl:matrix-rows matrix)))
+    (assert (and (evenp m) (= m (magicl:matrix-cols matrix)))
+            (matrix)
+            "Invalid matrix size.")
 
-      (let ((z (m- (m* c (magicl:conjugate-transpose u2) a4)
-                   (m* s (magicl:conjugate-transpose u1) a3))))
-        (multiple-value-bind (v2h y)
-            (polar-decomposition z :mode :hermitian-unitary)
-          (declare (ignorable y))
-          (values u1 u2 c s v1 (magicl:conjugate-transpose v2h)))))))
+    (assert (and (= p q) (or (= p 1) (= p (/ m 2))))
+            (p q)
+            "This implementation of the CS decomposition supports equipartitions or partitions with (p, q) = (1, 1) only.")
 
-(defun csd-compat (x p q)
-  "Drop-in replacement for LAPACK-CSD. Assumes X a square 2n×2n matrix and p = q = n."
-  (assert (and (= p q) (= (* 2 p) (magicl:matrix-rows x))))
+    (let ((equipartition-p (= p (/ m 2)))
+          (a1 (magicl:slice matrix 0 p 0 q))
+          (a2 (magicl:slice matrix p m 0 q))
+          (a3 (magicl:slice matrix 0 p q m))
+          (a4 (magicl:slice matrix p m q m)))
 
-  (multiple-value-bind (u1 u2 c s v1 v2)
-      (csd-2x2 x)
+      (multiple-value-bind (u1 u2 c s v2h)
+          (csd-2x1 a3 a4)
 
-    (let ((thetas (map 'list
-                       (lambda (x y)
-                         (atan (realpart y) (realpart x)))
-                        (magicl:matrix-diagonal c) (magicl:matrix-diagonal s))))
+        (let ((theta (map 'list
+                          (lambda (x y)
+                            (atan (realpart y) (realpart x)))
+                          (subseq (magicl:matrix-diagonal c) 0 p)
+                          (subseq (magicl:matrix-diagonal s) 0 p))))
 
-      (values u1 u2 (magicl:conjugate-transpose v1) (magicl:conjugate-transpose v2) thetas))))
+          (multiple-value-bind (v1h y)
+              (let ((z (m+ (m* c (magicl:conjugate-transpose u1) a1)
+                           (m* s (magicl:conjugate-transpose u2) a2))))
+                (polar-decomposition z :mode :hermitian-unitary))
+            (declare (ignorable y))
+
+            (flet ((make-complex-1x1-matrix (matrix)
+                     "Return complex 1x1 matrix whose sole entry is the first component of the first row of MATRIX."
+                     (magicl:make-complex-matrix 1 1 (list (magicl:ref matrix 0 0)))))
+
+              (values (if equipartition-p
+                          u1
+                          (make-complex-1x1-matrix u1))
+                      u2
+                      (if equipartition-p
+                          v1h
+                          (make-complex-1x1-matrix v1h))
+                      v2h
+                      theta))))))))
 
 (defparameter *lapack-csd* (if (magicl.foreign-libraries:foreign-symbol-available-p "zuncsd_" 'magicl.foreign-libraries:liblapack)
                                #'magicl:lapack-csd
-                               #'csd-compat))
+                               #'csd)
+  "Implementation of the CS decomposition suitable for use in Quilc. Allows us to replace ZUNCSD on systems whose LAPACK does not provide the function.")
