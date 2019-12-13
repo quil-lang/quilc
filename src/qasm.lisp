a0239bffd1ba9e8745e00d85a0fddf256e6aea30
Mark Skilbeck <mark.skilbeck@rigetti.com>
2019-12-13 15:42:39 -0800

    Revert "Remove unnecessary IDENT token"
    This reverts commit 9968f4ba9002ffb19d451e89f6d1315ce9a302b9.

--- src/qasm.lisp
+++ src/qasm.lisp
@@ -107,6 +107,7 @@
   "A lexical analyzer for lines of OpenQASM 2.0."
   ((:int    "\\d+")
    (:real  "(?=\\d*[.eE])(?=\\.?\\d)\\d*\\.?\\d*(?:[eE][+-]?\\d+)?")
+   (:ident  "[a-zA-Z](?:[A-Za-z0-9_\\-]*[A-Za-z0-9_])?")
    (:string "\\\"(?:[^\\\"]|\\\\\\\")*\\\"")
    (:newline "(?:\\r\\n?|\\n)"))
   ;; TODO Do I want eager here?
@@ -154,6 +155,8 @@
    (return (tok :REAL (parse-float:parse-float $@))))
   ((eager "{{INT}}")
    (return (tok :NNINTEGER (parse-integer $@))))
+  ("{{IDENT}}"
+   (return (tok :ID $@)))
   ("[^\\S\\n\\r]+"
    nil))
 
