4ab866d2d1484cf7634582d4dbf85be01c51534b
Eric Peterson <eric@rigetti.com>
2019-12-30 21:51:12 -0800

    more PR feedback

--- benchmarking/qasm-benchmarks.lisp
+++ benchmarking/qasm-benchmarks.lisp
@@ -11,6 +11,36 @@
   (or (uiop:directory-files *qasm-benchmark-path* #P"*.qasm")
       (error "QASM test files not found in ~a" *qasm-benchmark-path*)))
 
+(defun idiomatic-package-variables (package &key include-internal)
+  (let ((symbols nil))
+    (labels ((bookended-by (char symbol)
+               (let* ((string (symbol-name symbol))
+                      (len (length string)))
+                 (and (<= 2 len)
+                      (char= char (char string 0) (char string (1- len))))))
+             (variable-looking-symbol-p (s)
+               (and (boundp s)
+                    (or (bookended-by #\+ s)
+                        (bookended-by #\* s))))
+             (ponder-symbol (s)
+               (when (variable-looking-symbol-p s)
+                 (push s symbols))))
+      (do-symbols (s package (sort symbols #'string<))
+        (multiple-value-bind (s status)
+            (find-symbol (symbol-name s) package)
+          (case status
+            (:internal
+             (when include-internal
+               (ponder-symbol s)))
+            (:external
+             (ponder-symbol s))))))))
+
+(defun print-configuration ()
+  (format t "Current CL-QUIL configuration:~%")
+  (dolist (symb (idiomatic-package-variables ':cl-quil :include-internal t))
+    (format t "~a: ~a~%" symb (eval symb)))
+  (format t "~%"))
+
 (defun trim-long-string (string length)
   (if (>= length (length string))
       string
@@ -29,11 +59,13 @@
 (defun benchmark-qasm-suite (&key (timeout 30))
   (flet ((print-rule ()
            (format t "+------------------+----------+-------+----------+~%")))
+    (print-configuration)
     (print-rule)
     (format t "|       NAME       | TIME (s) | SWAPS | 2Q DEPTH |~%")
     (print-rule)
     (let ((chip (quil::build-ibm-qx5))
-          (quil::*default-addresser-state-class* 'quil::temporal-addresser-state))
+          (quil::*default-addresser-state-class* 'quil::temporal-addresser-state)
+          (quil::*safe-include-directory* (asdf:system-relative-pathname :cl-quil "tests/qasm-files/")))
       (dolist (file (qasm-test-files))
         (format t "| ~Va " 16 (trim-long-string (pathname-name file) 16))
         (finish-output)
@@ -44,7 +76,7 @@
                 (with-stopwatch elapsed-time
                   (multiple-value-bind (cpp swaps)
                       (quil::compiler-hook (quil::parse text
-                                                        :originating-file (first (directory file)))
+                                                        :originating-file file)
                                            chip
                                            :protoquil t)
                     (format t "| ~Vf | ~Vd | ~Vd |~%"
