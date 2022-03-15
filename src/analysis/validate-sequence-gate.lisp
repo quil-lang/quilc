27ce0acdd39db7e4cb2b15c1aef56ce695510897
Parker Williams <pawilliams@hrl.com>
2022-03-15 15:56:54 -0700

    more descriptive errors

--- src/analysis/validate-sequence-gate.lisp
+++ src/analysis/validate-sequence-gate.lisp
@@ -11,12 +11,12 @@
     (labels ((validate-argument (arg)
                (cond
                  ((qubit-p arg)
-                  (quil-parse-error "argument cannot be qubit"))
+                  (quil-parse-error "Argument of qubit is not allowed: ~a" arg))
                  ((not (is-formal arg))
-                  (quil-parse-error "argument is not formal"))
+                  (quil-parse-error "Argument must be formal: ~a" arg))
                  ((not (find arg args :test #'formal=))
-                  (quil-parse-error "argument ~a is not in gatedef arguments" arg))
-                 (T T))))
+                  (quil-parse-error "Argument ~a is not in gate definition arguments" arg))
+                 (t t))))
       (unless (seq-contains-only-gate-definitions-p seq)
         (quil-parse-error (format nil "Sequence gate definition body (~a) contains non gate-application" (gate-definition-name gate-def) )))
       (loop :for isn :in seq
@@ -36,10 +36,10 @@
 (defun verify-no-loops-in-gate-defs (gate-defs &optional (ancestors NIL))
   "Takes list of gate defs, verifys there are no circular dependencies between sequence gate defs."
   (dolist (gate-def gate-defs)
-    (when (typep gate-def 'SEQUENCE-GATE-DEFINITION)
+    (when (typep gate-def 'sequence-gate-definition)
       (let ((gate-name (gate-definition-name gate-def)))
         (if (member gate-name ancestors)
-            (quil-parse-error (format nil "Defgate sequence dependencies contains a loop: ~a " (reverse (cons gate-name ancestors))))
+            (quil-parse-error (format nil "DEFGATE AS SEQUENCE dependencies contains a loop: ~a " (reverse (cons gate-name ancestors))))
             (verify-no-loops-in-gate-defs (neighbors-of-sequence-gate-def gate-def) (cons gate-name ancestors)))))))
 
 (defun neighbors-of-sequence-gate-def (gate-def)
