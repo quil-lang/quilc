11c2d9d7a8a263f96af7af5cf0d2a17405537947
Kartik Singh <kssingh@hrl.com>
2022-09-12 11:52:37 -0700

    Update package names for libquilc exported functions

--- lib/src/libquilc.lisp
+++ lib/src/libquilc.lisp
@@ -20,11 +20,11 @@
   (:type quil-program chip-specification error-type)
   (:literal "/* functions */")
   (:function
-   (("parse_quil" quil:safely-parse-quil) quil-program ((source :string)))
-   (("print_program" quilc::print-program) :void ((program quil-program)))
-   (("compile_quil" quil:compiler-hook) quil-program ((program quil-program) (chip-spec chip-specification)))
+   (("parse_quil" cl-quil.frontend:safely-parse-quil) quil-program ((source :string)))
+   (("print_program" cl-quil.frontend:print-parsed-program) :void ((program quil-program)))
+   (("compile_quil" cl-quil:compiler-hook) quil-program ((program quil-program) (chip-spec chip-specification)))
    (("compile_protoquil" compile-protoquil) quil-program ((program quil-program) (chip-spec chip-specification)))
-   (("build_nq_linear_chip" quil::build-nq-linear-chip) chip-specification ((n :int)))
+   (("build_nq_linear_chip" cl-quil::build-nq-linear-chip) chip-specification ((n :int)))
    (("chip_spec_from_isa_descriptor" quilc::lookup-isa-descriptor-for-name) chip-specification ((descriptor :string)))
-   (("print_chip_spec" quil::debug-print-chip-spec) :void ((chip-spec chip-specification)))))
+   (("print_chip_spec" cl-quil::debug-print-chip-spec) :void ((chip-spec chip-specification)))))
 
