2b211bba36f3697c44f55c14db552c29d20fde61
Kartik Singh <kssingh@hrl.com>
2022-09-19 15:59:11 -0700

    Increase heap size when initializing runtime for libquilc

--- lib/src/build-image.lisp
+++ lib/src/build-image.lisp
@@ -8,6 +8,6 @@
 (sbcl-librarian:define-aggregate-library libquilc (:function-linkage "QUILC_API")
   quilc sbcl-librarian:handles)
 
-(sbcl-librarian:build-bindings libquilc ".")
+(sbcl-librarian:build-bindings libquilc "." :initialize-lisp-args '("--dynamic-space-size" "8192"))
 (sbcl-librarian:build-python-bindings libquilc ".")
 (sbcl-librarian:build-core-and-die libquilc ".")
